ba90eff378df6fe0f330c0b2498ab63907ef1977
Dat72 <dattran250301@gmail.com>
2025-02-15 14:24:53 +0700

    Cập nhật form1

--- BaitapTH/Form1.Designer.cs
+++ BaitapTH/Form1.Designer.cs
@@ -34,6 +34,7 @@
 			this.textBox1 = new System.Windows.Forms.TextBox();
 			this.textBox2 = new System.Windows.Forms.TextBox();
 			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
 			// label1
@@ -41,9 +42,9 @@
 			this.label1.AutoSize = true;
 			this.label1.Location = new System.Drawing.Point(123, 84);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(13, 13);
+			this.label1.Size = new System.Drawing.Size(27, 13);
 			this.label1.TabIndex = 0;
-			this.label1.Text = "a";
+			this.label1.Text = "số a";
 			this.label1.Click += new System.EventHandler(this.label1_Click);
 			// 
 			// label2
@@ -51,9 +52,9 @@
 			this.label2.AutoSize = true;
 			this.label2.Location = new System.Drawing.Point(122, 123);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(13, 13);
+			this.label2.Size = new System.Drawing.Size(27, 13);
 			this.label2.TabIndex = 1;
-			this.label2.Text = "b";
+			this.label2.Text = "số b";
 			// 
 			// label3
 			// 
@@ -70,6 +71,7 @@
 			this.textBox1.Name = "textBox1";
 			this.textBox1.Size = new System.Drawing.Size(100, 20);
 			this.textBox1.TabIndex = 3;
+			this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
 			// 
 			// textBox2
 			// 
@@ -85,11 +87,22 @@
 			this.textBox3.Size = new System.Drawing.Size(100, 20);
 			this.textBox3.TabIndex = 5;
 			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(159, 203);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 6;
+			this.button1.Text = "Cộng";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.button1);
 			this.Controls.Add(this.textBox3);
 			this.Controls.Add(this.textBox2);
 			this.Controls.Add(this.textBox1);
@@ -111,6 +124,7 @@
 		private System.Windows.Forms.TextBox textBox1;
 		private System.Windows.Forms.TextBox textBox2;
 		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Button button1;
 	}
 }
 
